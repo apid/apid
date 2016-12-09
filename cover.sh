21fa75e4ef2adcc57580e7505336e706729a1ee0
Paul Kim <pkim@apigee.com>
2016-12-09 11:16:18 -0800

    Fix coverage script for jenkins build
    On OS X 'tail +2 <foo>' works, but not on linux

--- cover.sh
+++ cover.sh
@@ -44,7 +44,7 @@ cd $DIR
 for d in $(go list ./... | grep -v vendor); do
     go test -coverprofile=profile.out -covermode=$coverMode $d
     if [ -f profile.out ]; then
-        tail +2 profile.out >> coverage.txt
+        tail -n+2 profile.out >> coverage.txt
         rm profile.out
     fi
 done
