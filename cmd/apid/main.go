4f2765c68f306c9f95afb59ff65a471ae1c8eb20
Scott Ganyo <scott@ganyo.com>
2017-01-18 10:17:21 -0800

    Add a -clean flag to the cmd

--- cmd/apid/main.go
+++ cmd/apid/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	// import plugins to ensure they're bound into the executable
 	_ "github.com/30x/apidApigeeSync"
-	_ "github.com/30x/apidVerifyAPIKey"
+	//_ "github.com/30x/apidVerifyAPIKey"
 	_ "github.com/30x/apidGatewayDeploy"
 
 	// other imports
@@ -15,17 +15,29 @@ import (
 
 func main() {
 	configFlag := flag.String("config", "", "path to the yaml config file [./apid_config.yaml]")
-
-	flag.Parse()
+	cleanFlag := flag.Bool("clean", false, "start clean, deletes all existing data from local_storage_path")
 
 	configFile := *configFlag
 	if configFile != "" {
 		os.Setenv("APID_CONFIG_FILE", configFile)
 	}
 
+	flag.Parse()
+
 	apid.Initialize(factory.DefaultServicesFactory())
 
 	log := apid.Log()
+	config := apid.Config()
+
+	if *cleanFlag {
+		localStorage := config.GetString("local_storage_path")
+		log.Infof("removing existing data from: %s", localStorage)
+		err := os.RemoveAll(localStorage)
+		if err != nil {
+			log.Panic("Failed to clean data directory: %v", err)
+		}
+	}
+
 	log.Debug("initializing...")
 
 	apid.InitializePlugins()
