2fa65ae3da941d6f479020a2baf9f654d5c6aed1
Haoming Zhang <zhm1993zhm@gmail.com>
2017-09-14 10:18:35 -0700

    [ISSUE-65203841] update dockerfile

--- load-test/startMockserver.sh
+++ load-test/startMockserver.sh
@@ -15,6 +15,4 @@
 # limitations under the License.
 
 #!/bin/bash
-
-export BUNDLE_URI="http://$CLOUD_IP:$APID_API_PORT/bundles/1"
-./mockServer -numDeps=100 -numDevs=50000 -addDevEach=3s -upDevEach=1s -upDepEach=3s -bundleURI=$BUNDLE_URI
+./mock_server
