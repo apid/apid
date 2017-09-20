6c73408894704900853b1e32f0f427fdbbd4969e
Haoming Zhang <zhm1993zhm@gmail.com>
2017-09-19 21:21:00 -0700

    [ISSUE-65203841]

---
+++ load-test/artillery/configurations.js
@@ -0,0 +1,32 @@
+// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//
+//
+module.exports = {
+  captureConfigurations: captureConfigurations,
+}
+
+function captureConfigurations(requestParams, response, context, ee, next) {
+	if (response.statusCode != 200) {
+		console.error("error response:", response.statusCode);
+		return next();
+	}
+	var d = JSON.parse(response.body);
+	if (d.contents.length != 30) {
+		console.error("error response:", d);
+		return next();
+	}
+	return next(); // MUST be called for the scenario to continue
+}
